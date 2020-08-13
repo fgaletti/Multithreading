2bf9cd38f8fe77f23cee522e6186e0eddebc63a9
francisco <franciscogaletti@yahoo.com>
2020-08-13 17:33:59 -0500

    7_QueueUserWorkItem

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -7,39 +7,21 @@ using System.Threading.Tasks;
 
 namespace Multithreading
 {
+    
+      
+     //7_QueueUserWorkItem
     class Program
     {
-        //  5_Thread_Static
-        [ThreadStatic]
-        public static int _field; // unique for every field 
-
         static void Main(string[] args)
         {
-            // thread 1
-            Thread t1 = new Thread(new ThreadStart(() =>
+            ThreadPool.QueueUserWorkItem((s) =>
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    _field++;
-                    Console.WriteLine("Thread A: {0}", _field);
-                }
+                Console.WriteLine("Working on a thread from threadpool..");
             }
-            ));
-            t1.Start();
+            );
 
-            // thread 2
-            Thread t2 = new Thread(new ThreadStart(() =>
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    _field++;
-                    Console.WriteLine("Thread BB: {0}", _field);
-                }
-            }
-            ));
-            t2.Start();
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
+    }
     }
 }
