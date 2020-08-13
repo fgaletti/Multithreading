761107eb0c010bcab27c657ee40e12ace7870788
francisco <franciscogaletti@yahoo.com>
2020-08-13 17:11:48 -0500

    3_Parametereizer_Thread

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -9,32 +9,21 @@ namespace Multithreading
 {
     class Program
     {
-        // foreground / background
-        public static void ThreadMethod()
+        public static void ThreadMethod(Object o)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < (int)o; i++)
             {
-                Console.WriteLine("ThreadProc2 {0} ", i);
-                Thread.Sleep(1000); // background thread
-            }
-        }
-        static void Main(string[] args)
-        {
-            Thread t = new Thread(new ThreadStart(ThreadMethod));
-            t.IsBackground = true; // It does not prevent the program from closing 
-            t.Start();
-
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine("Main Thread: Do some work");
+                Console.WriteLine("ThreadProc {0} ", i);
                 Thread.Sleep(0); // foreground thread
             }
+        }
 
-            t.Join(); // prevent the program to close as soon as we exit the method "For" 
-                      // don go pass this line of code Until the thread we executed is Done Executing its code
-                      //Always wait for all the thread to finish
-
-            Console.WriteLine("AFTER");
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(15);
+            t.Join();
+            Console.ReadLine();
         }
     }
 }
