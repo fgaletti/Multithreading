10d837a8b8cf4376c6d556904478ffbcaebdfacd
francisco <franciscogaletti@yahoo.com>
2020-08-14 00:13:05 -0500

    12 Task.WaitAny

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -7,45 +7,50 @@ using System.Threading.Tasks;
 
 namespace Multithreading
 {
-    //11 Task.WaitAll
+    //12 Task.WaitAny
     class Program
     {
         static void Main(string[] args)
         {
-            Task[] tasks = new Task[3];
-
-            // 2 case returning INT  : 
-            // Task<int>[] tasks = new Task<int>[3];
+            Task<int>[] tasks = new Task<int>[3];
 
             tasks[0] = Task.Run(() =>
             {
-                Thread.Sleep(1000);
-                Console.WriteLine("1");
-                return 111;
+                Thread.Sleep(2000);
+                return 1;
             });
 
             tasks[1] = Task.Run(() =>
             {
                 Thread.Sleep(2000);
-                Console.WriteLine("2");
                 return 2;
             });
 
             tasks[2] = Task.Run(() =>
             {
-                Thread.Sleep(3000);
-                Console.WriteLine("3");
+                Thread.Sleep(2000);
                 return 3;
             });
 
-            // Activate 2 case to get the result -> Console.WriteLine(tasks[0].Result);
-            
-             Task.WaitAll(tasks); // wait for all threads to finish
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks); // wait for any Task that is completed, it can be Any 1,2,3 in an unpreditable order      
+                Task<int> completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+                var temp = tasks.ToList();
+                temp.RemoveAt(i); // remove the task that is completed
+                tasks = temp.ToArray();
+            }
 
-            //after WaitAll
 
-            Console.WriteLine("After WaitAll");
+            /* List OF TASK String */
+            /* Task<string>[] tasksStrings = new Task<string>[3];
+             tasksStrings[0] = Task.Run(() =>
+             {
+                 Thread.Sleep(2000);
+                 return "String";
+             });*/
+
         }
     }
-
 }
