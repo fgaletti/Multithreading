7b217da2d27f7b87cbbe44c005a36ce6f1201d7e
francisco <franciscogaletti@yahoo.com>
2020-08-14 03:09:37 -0500

    37_Creating a deadlock

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -8,49 +8,38 @@ using System.Threading.Tasks;
 
 namespace Multithreading
 {
-    //35 Share Data Multithread 
+    // 37_Creating a deadlock
+    // where both thread are waiting for each other
     class Program
     {
-
-        // non-atomic 
         static void Main(string[] args)
         {
-            int n = 0;
-
-            object _lock = new object(); //Ensure that n = 0, Make the thread atomic
+            object lockA = new object();
+            object lockB = new object();
 
             Task myTask = Task.Run(() =>
             {
-                for (int i = 0; i < 1000000; i++)
+                lock (lockA)
                 {
-                    lock(_lock)
+                    Thread.Sleep(5000); //We could omit this line
+                    lock (lockB)
                     {
-                        n++;
+                        Console.WriteLine("Locked A and B");
                     }
-                   // n++;
-                    // n++ -> is reading and writing
-                    // n = n + 1 ;
-                    // 1 step : reading n
-                    // 2 step : sum  of n + 1    
-                    // 3 step: after the step 2 n could have been updated for the other thread n = n -1 or more that one time n -1 ; n -1 ;
-                    // 4 step : assign the sum of (n + 1 ) -> n (but n was modified by the second thread)
-                    //                                        maybe 1 or multiple times  
-
                 }
-            });
+            }
+            );
 
-            for (int i = 0; i < 1000000; i++)
+            lock (lockB)
             {
-                lock (_lock)
+                Thread.Sleep(1000);
+                lock (lockA)
                 {
-                    n--;
+                    Console.WriteLine("Locked B and A");
                 }
-               // n--;
             }
 
             myTask.Wait();
-            Console.WriteLine(n);
-
         }
     }
 }
