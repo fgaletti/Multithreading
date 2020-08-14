323461c27b6c31480aefaeeff29c8f98edea4e19
francisco <franciscogaletti@yahoo.com>
2020-08-14 00:36:36 -0500

    18_Asycn / Await

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -1,47 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Multithreading
 {
-    //16 Parallel.For / ForEach
+    //18 Asycn / Await
     class Program
     {
         static void Main(string[] args)
         {
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine("Value of {0}",i);
-            //    Thread.Sleep(1000);
-            //}
-
-            Thread.Sleep(2000);
-            Console.WriteLine("Start");
-
-            // FOR ------
-            Parallel.For(0, 20, (i) =>
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(1000);
-            });
-
-            Console.ReadKey();
-            //FOR EACH ----
-            Console.Clear();
-
-            Thread.Sleep(2000);
-            Console.WriteLine("Start");
-            int[] myArray = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 100 };
-
-            Parallel.ForEach(myArray, (i) =>
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(1000);
-            });
-
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
         }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("https://www.microsoft.com");
+                return result;
+            }
+        }
+
+
     }
 }
