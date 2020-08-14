45b3529879603311c3636371ea4321fd23768ae9
francisco <franciscogaletti@yahoo.com>
2020-08-14 02:47:30 -0500

    35_Share_Data_Multithread

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -8,24 +8,49 @@ using System.Threading.Tasks;
 
 namespace Multithreading
 {
-    //18 Asycn / Await
+    //35 Share Data Multithread 
     class Program
     {
+
+        // non-atomic 
         static void Main(string[] args)
         {
-            string result = DownloadContent().Result;
-            Console.WriteLine(result);
-        }
+            int n = 0;
 
-        public static async Task<string> DownloadContent()
-        {
-            using (HttpClient client = new HttpClient())
+            object _lock = new object(); //Ensure that n = 0, Make the thread atomic
+
+            Task myTask = Task.Run(() =>
             {
-                string result = await client.GetStringAsync("https://www.microsoft.com");
-                return result;
+                for (int i = 0; i < 1000000; i++)
+                {
+                    lock(_lock)
+                    {
+                        n++;
+                    }
+                   // n++;
+                    // n++ -> is reading and writing
+                    // n = n + 1 ;
+                    // 1 step : reading n
+                    // 2 step : sum  of n + 1    
+                    // 3 step: after the step 2 n could have been updated for the other thread n = n -1 or more that one time n -1 ; n -1 ;
+                    // 4 step : assign the sum of (n + 1 ) -> n (but n was modified by the second thread)
+                    //                                        maybe 1 or multiple times  
+
+                }
+            });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_lock)
+                {
+                    n--;
+                }
+               // n--;
             }
+
+            myTask.Wait();
+            Console.WriteLine(n);
+
         }
-
-
     }
 }
